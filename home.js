5571f58891d2d2eec390ac86f533f352b0f94533
VladC0d <73534257+VladC0d@users.noreply.github.com>
2021-01-19 19:42:09 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,14 @@
+console.log('hello');
+//alert('yooooooooooooasdfgh');
+var b ='smoothie';
+console.log(b);
+
+var someNumber =45;
+console.log(someNumber);
+
+// document.getElementById('bunadimineata').innerHTML = 'ERROR 404';
+
+
+var age =prompt('What is your age');
+
+document.getElementById('buenosdias').innerHTML = age;
